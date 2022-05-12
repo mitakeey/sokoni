1a06d9ce4c74c72cf750619660f669da36dfda6c
mitakeey <mitakeeykiage@gmail.com>
2022-05-12 16:30:09 +0300

    :passport_control: constructors to fetch data

---
+++ scripts/categories.js
@@ -0,0 +1,15 @@
+/* fetch all categories of products from the api*/
+
+class categories{
+    constructor(){
+        this.apiUrl = 'https://fakestoreapi.com'
+    }
+
+    getAllCategories(){
+        /* Using ajax */
+        $.ajax({
+            type: 'GET',
+            url : this.apiUrl + 'products/categories'
+        })
+    }
+}
