5a55202f5b5a5b46da7640826f0dfa03c41f07e7
mitakeey <mitakeeykiage@gmail.com>
2022-05-13 16:33:53 +0300

    :triangular_flag_on_post: problems quering api reolve coming up

--- scripts/categories.js
+++ scripts/categories.js
@@ -5,19 +5,40 @@ class Categories{
         this.apiUrl = 'https://fakestoreapi.com/'
     }
 
-    getAllCategories(){
+    getAllCategories() {
+        /* Using ajax */
+		$.ajax({
+			type: "GET",
+			url: this.apiUrl + "products/categories",
+			success: function (data) {
+				$(data).each(function (index, category) {
+					$(".categories").append(
+						'<a class="dropdown-item" href="/category.html?category=' +
+							encodeURIComponent(category) +
+							'">' +
+							titleUppercase(category) +
+							"</a>"
+                    )
+                })
+            }
+        })
+    }
+
+    getonecategory(slug){
         /* Using ajax */
         $.ajax({
             type: 'GET',
-            url : this.apiUrl + 'products/categories',
+            url : this.apiUrl + 'products/category/electronics',
             success : function(data){
-                $(data).each(function(index, category) {
-                    $('.categories').append(
-                        '<a class="dropdown-item" href="/category.htm?category= '+ 
-                        encodeURIComponent(category)+
-                        '">'+ 
-                        titleUppercase(category)+
-                        '</a>'
+                console.log(data)
+                $(data).each(function(index, product) {
+                    $('.products').append(
+                        '<div class="col-md-4"><a href="/product.html?productid='+
+                        product.id +
+                        '"><img src="'+
+                        product.image+'"class="img=fluid">' +
+                        product.title +
+                        '</a></div'
                     )
                 })
             }
